488460fc0d854e5a6b8643122bdb4b725afb2f65
Yaroslav Nikonorov <yaroslav.n@quali.com>
2020-10-26 18:38:39 +0200

    Add save/restore for using scp only

--- cloudshell/checkpoint/gaia/flows/checkpoint_restore_flow.py
+++ cloudshell/checkpoint/gaia/flows/checkpoint_restore_flow.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
-
+from time import localtime
+import hashlib
 from cloudshell.devices.flows.action_flows import RestoreConfigurationFlow
+from cloudshell.devices.networking_utils import UrlParser
+
+from cloudshell.checkpoint.gaia.command_actions.save_restore_actions import SaveRestoreActions
 
 
 class CheckpointRestoreFlow(RestoreConfigurationFlow):
@@ -21,4 +24,30 @@ class CheckpointRestoreFlow(RestoreConfigurationFlow):
         :param vrf_management_name: Virtual Routing and Forwarding Name
         """
 
-        pass
+        with self._cli_handler.get_cli_service(self._cli_handler.enable_mode) as cli_service:
+            save_restore_actions = SaveRestoreActions(cli_service, self._logger)
+            url = UrlParser.parse_url(path)
+            scheme = url.get(UrlParser.SCHEME)
+            if scheme.lower() != "scp":
+                raise Exception("SCP only supported")
+            filename = url.get(UrlParser.FILENAME)
+
+            password = url.get(UrlParser.PASSWORD)
+            scp_target = "{user}@{hostname}:{filepath}/{filename}".format(
+                user=url.get(UrlParser.USERNAME),
+                hostname=url.get(UrlParser.HOSTNAME),
+                filepath=url.get(UrlParser.PATH),
+                filename=filename)
+
+            local_file = hashlib.sha1(str(localtime()).encode('utf-8')).hexdigest()
+
+            # download by scp
+            with cli_service.enter_mode(self._cli_handler.config_mode):
+                save_restore_actions.scp_transfer(scp_target, local_file, password)
+
+            # restore local
+            save_restore_actions.restore(local_file)
+
+            # remove local file
+            with cli_service.enter_mode(self._cli_handler.config_mode):
+                save_restore_actions.remove_local_file(local_file)
