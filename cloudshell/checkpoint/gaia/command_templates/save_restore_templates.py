bd4949e9adf28df373ddbc905c9b35c25f5fafc2
Yaroslav Nikonorov <yaroslav.n@quali.com>
2020-11-05 14:39:44 +0200

    Add set expert password if not defined

--- cloudshell/checkpoint/gaia/command_templates/save_restore_templates.py
+++ cloudshell/checkpoint/gaia/command_templates/save_restore_templates.py
@@ -2,22 +2,25 @@ from collections import OrderedDict
 
 from cloudshell.cli.command_template.command_template import CommandTemplate
 
-ERROR_MAP = OrderedDict([("Configuration lock present", "Configuration lock present."),
-                         ("Failed to maintain the lock", "Failed to maintain the lock."),
+ERROR_MAP = OrderedDict([("Failed to maintain the lock", "Failed to maintain the lock."),
                          ("Incomplete command", "Incomplete command.")])
 PASSWORD_ERROR_MAP = OrderedDict([
     ("must be at least", "Authentication pass phrase must be at least 8 characters"),
     ("not complex enough", "Password is not complex enough; try mixing more different kinds of characters "
                            "(upper case, lower case, digits, and punctuation)")])
 
+LOCK_ACTION_MAP = OrderedDict([(r"Configuration lock present",
+                                lambda s, l: s.send_line("lock database override", l))])
+
 # Restore
-ON_FAILURE_CONTINUE = CommandTemplate("set clienv on-failure continue", error_map=ERROR_MAP)
-LOAD_CONFIGURATION = CommandTemplate("load configuration {filename}", error_map=ERROR_MAP)
-ON_FAILURE_STOP = CommandTemplate("set clienv on-failure stop", error_map=ERROR_MAP)
-SAVE_CONFIG = CommandTemplate("save config", error_map=ERROR_MAP)
+ON_FAILURE_CONTINUE = CommandTemplate("set clienv on-failure continue", error_map=ERROR_MAP, action_map=LOCK_ACTION_MAP)
+LOAD_CONFIGURATION = CommandTemplate("load configuration {filename}", error_map=ERROR_MAP, action_map=LOCK_ACTION_MAP)
+ON_FAILURE_STOP = CommandTemplate("set clienv on-failure stop", error_map=ERROR_MAP, action_map=LOCK_ACTION_MAP)
+SAVE_CONFIG = CommandTemplate("save config", error_map=ERROR_MAP, action_map=LOCK_ACTION_MAP)
 
 # Save
-SAVE_CONFIGURATION = CommandTemplate("save configuration {filename}", error_map=ERROR_MAP)
+SAVE_CONFIGURATION = CommandTemplate("save configuration {filename}",
+                                     error_map=ERROR_MAP, action_map=LOCK_ACTION_MAP)
 
 # RM file
 REMOVE = CommandTemplate("rm {filename}")
